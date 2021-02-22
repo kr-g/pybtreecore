98be012d965f19edf5bc57c809985e7f399a65fc
kr-g <k.r.goger@gmail.com>
2021-02-22 07:59:51 +0100

    refactor

--- pybtreecore/btnodelist.py
+++ pybtreecore/btnodelist.py
@@ -76,8 +76,7 @@ class NodeList(object):
     def _split(buf, blen):
         return buf[:blen], buf[blen:]
 
-    def from_bytes(self, buf, pos=0):
-        self.pos = pos
+    def from_bytes(self, buf):
         while len(buf) > 0:
             n = Node()
             res = n.from_bytes(buf)
